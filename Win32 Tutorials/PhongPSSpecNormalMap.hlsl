c28231bdbe943989a174f9effa783238119c4c63
Adam Harris <adam_harris@hotmail.com>
2020-05-02 15:17:42 +0100

    REFACTOR: Pulled more functionality into seperate methods

--- Win32 Tutorials/PhongPSSpecNormalMap.hlsl
+++ Win32 Tutorials/PhongPSSpecNormalMap.hlsl
@@ -25,52 +25,82 @@ Texture2D nmap;
 
 SamplerState splr;
 
-float3 MapNormalViewSpace(const float3 tan, const float3 bitan, const float3 viewNormal, const float2 tc, Texture2D nmap, SamplerState splr)
+float3 MapNormal(
+    const in float3 tan, 
+    const in float3 bitan, 
+    const in float3 normal,
+    const in float2 tc, 
+    uniform Texture2D nmap, 
+    uniform SamplerState splr)
 {
-    const float3x3 tanToView = float3x3(
+    const float3x3 tanToTarget = float3x3(
             normalize(tan),
             normalize(bitan),
-            normalize(viewNormal)
+            normalize(normal)
         );
         
     const float3 normalSample = nmap.Sample(splr, tc).xyz;
     const float3 tanNormal = normalSample * 2.0f - 1.0f;
         
-    return normalize(mul(tanNormal, tanToView));
+    return normalize(mul(tanNormal, tanToTarget));
 }
 
+float Attenuate(uniform float attConst, uniform float attLin, uniform float attQuad, const in float distFragToL)
+{
+    return 1.0f / (attConst + attLin * distFragToL + attQuad * (distFragToL * distFragToL));
+}
+
+float3 Diffuse(
+    uniform float3 diffuseColor,
+    uniform float diffuseIntensity,
+    const in float att,
+    const in float3 viewDirFragToL,
+    const in float3 viewNormal)
+{
+    return diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirFragToL, viewNormal));
+}
+
+float3 Speculate(
+    uniform float3 specularColor,
+    uniform float specularIntensity,
+    const in float3 viewNormal,
+    const in float3 viewFragToL,
+    const in float3 viewPos,
+    const in float att,
+    const in float specularPower)
+{
+        //Reflected Light Vector
+    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
+    const float3 r = normalize(w * 2.0f - viewFragToL);
+    
+    const float3 viewCamToFrag = normalize(viewPos);
+    
+    return att * specularColor * specularIntensity * pow(max(0.0f, dot(-r, viewCamToFrag)), specularPower);
+    
+}
 
 float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tan : Tangent, float3 bitan : Bitangent, float2 tc : Texcoord) : SV_Target
 {
     if (normalMapEnabled)
     {
-        viewNormal = MapNormalViewSpace(tan, bitan, viewNormal, tc, nmap, splr);
+        viewNormal = MapNormal(tan, bitan, viewNormal, tc, nmap, splr);
     }
     
     //Fragment to light vector data
     const float3 viewFragToL = viewLightPos - viewPos;
     const float distFragToL = length(viewFragToL);
-    const float3 viewDirToL = viewFragToL / distFragToL;
+    const float3 viewDirFragToL = viewFragToL / distFragToL;
     
-    //Attenuation
-    const float att = 1.0f / (attConst + attLin * distFragToL + attQuad * (distFragToL * distFragToL));
+    const float att = Attenuate(attConst, attLin, attQuad, distFragToL);
     
-    //Diffuse Intensity
-    const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(viewDirToL, viewNormal));
-    
-    //Reflected Light Vector
-    const float3 w = viewNormal * dot(viewFragToL, viewNormal);
-    const float3 r = w * 2.0f - viewFragToL;
-    
-    //Calculate Specular intensity
-    const float4 specularSample = spec.Sample(splr, tc);
+    const float3 diffuse = Diffuse(diffuseColor, diffuseIntensity, att, viewDirFragToL, viewNormal);
+
     float3 specularReflectionColor;
     float specularPower = specularPowerConst;
     if (specularMapEnabled)
     {
         const float4 specularSample = spec.Sample(splr, tc);
         specularReflectionColor = specularSample.rgb * specularMapWeight;
-        
         if (hasGloss)
         {
             specularPower = pow(2.0f, specularSample.a * 13.0f);
@@ -80,11 +110,13 @@ float4 main(float3 viewPos : Position, float3 viewNormal : Normal, float3 tan : 
     {
         specularReflectionColor = specularColor;
     }
-
-
-    const float3 specular = att * (diffuseColor * diffuseIntensity) * pow(max(0.0f, dot(normalize(-r), normalize(viewPos))), specularPower);
+    
+    const float3 specularReflected = Speculate(
+        specularReflectionColor, 1.0f, viewNormal,
+        viewFragToL, viewPos, att, specularPower
+    );
   
     //Calculate final colour with texture and diffuse color
-    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular * specularReflectionColor), 1.0f);
+    return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specularReflected), 1.0f);
 
 }
